486213a3abd3ed7b359de3f412a4e3c05825d68b
genepjs0719-lgtm <genepjs0719@gmail.com>
2025-10-17 10:48:53 +0900

    20251017

--- Assets/01.Scripts/PlayerMovement.cs
+++ Assets/01.Scripts/PlayerMovement.cs
@@ -9,14 +9,21 @@ public class PlayerMovement : MonoBehaviour {
     private PlayerInput playerInput; // 플레이어 입력을 알려주는 컴포넌트
     private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
     private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
-
+    
     private void Start() {
-        // 사용할 컴포넌트들의 참조를 가져오기
+        // 사용
+    playerInput = GetComponent<PlayerInput>(); 
+    playerRigidbody = GetComponent<Rigidbody>(); 
+    playerAnimator = GetComponent<Animator>();
     }
 
     // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
     private void FixedUpdate() {
         // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
+        Rotate();
+        Move();
+        playerAnimator.SetFloat("Move", playerInput.move);
+
     }
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임
